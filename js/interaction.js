aa3b91b5cc077a1aa9e0ada266ebd9f7a52c1290
AlessandroRomanelli <aleromrod@gmail.com>
2017-02-11 18:52:26 +0100

    Form validation added

--- js/interaction.js
+++ js/interaction.js
@@ -7,10 +7,8 @@
  $(document).ready(function(){
    //Give focus to the first text field
    $("input[type=text]")[0].focus();
-   //Give the credit-card number a mask to reduce user input errors
-   jQuery(function($){
-   $("#cc-num").mask("9999-9999-9999-9999", {placeholder:"XXXX-XXXX-XXXX-XXXX"});
-});
+   $("form").children().last().prop("disabled", true);
+  $("form").children().last().addClass("disabled");
  });
 
 // ===== 2nd task: ”Job Role” section of the form:
@@ -75,10 +73,8 @@ $("select#design").change(function(){
 
 // ===== 4th task: ”Register for Activities” section of the form:
 
-//When a checkbox changes status
 var $checkbox = $(".activities input");
-var checkedClass = "";
-
+//When a checkbox changes status
 $checkbox.change(function(){
 //(Re)Calculate total purchase:
   var money = 0,
@@ -94,39 +90,138 @@ $checkbox.change(function(){
   $(".activities span").remove();
   $(".activities").append($moneySpan);
 
-  //If there is atleast one element checked on tuesday morning
-  if ($(".tuesday.am:checked").length > 0){
-    //Then disable each element
-    $(".tuesday.am").each(function(){
+//Declaring a function to exclude conflicting dayTimes
+function checkboxEsclusion (dayTime) {
+  //If there is atleast one checked element
+  if ($(dayTime+":checked").length > 0){
+    //Then disable each element with same dayTime
+    $(dayTime).each(function(){
       $(this).prop("disabled", true);
-      $(this).parent().addClass("error");
+      //Add
+      $(this).parent().addClass("conflict");
     });
   } else {
     //Else enable each element
-    $(".tuesday.am").each(function(){
+    $(dayTime).each(function(){
       $(this).prop("disabled", false);
-      $(this).parent().removeClass("error")
+      $(this).parent().removeClass("conflict")
     });
   }
-  //If there is atleast one element on tuesday afternoon
-  if ($(".tuesday.pm:checked").length > 0){
-    $(".tuesday.pm").each(function(){
-      $(this).prop("disabled", true)
-      $(this).parent().addClass("error");
-    })
-  } else {
-    $(".tuesday.pm").each(function(){
-      $(this).prop("disabled", false);
-      $(this).parent().removeClass("error")
-    })
-  }
+}
+  //Call the function on tuesday morning and afternoon elements
+  checkboxEsclusion(".tuesday.am");
+  checkboxEsclusion(".tuesday.pm");
   //Keep the already checked items enabled
   $(".tuesday:checked").prop("disabled", false);
-  $(".tuesday:checked").parent().removeClass("error")
+  $(".tuesday:checked").parent().removeClass("conflict")
 });
 
-// ===== 5th task: ”Register for Activities” section of the form:
+// ===== 5th task: Payment Info section of the form:
 
 //Hide all payment methods but the credit-card method, default
-//Based on the value of the payment method select
-  //Display only one at a time
+$("#credit-card").nextAll().hide();
+//When payment select changes option
+$("#payment").change(function(){
+  //If paypal is selected,
+  if ($("#payment").val() == "paypal") {
+    //Hide all elements
+    $("#payment").nextAll().slideUp();
+    //..but PayPal!
+    $("#credit-card").next().slideDown();
+    //Else if bitcoin is selected
+  } else if ($("#payment").val() == "bitcoin") {
+    //Hide all elements
+    $("#payment").nextAll().slideUp();
+    //... but BitCoin!
+    $("#payment").nextAll().last().slideDown();
+    //Otherwise if credit card is selected AND the div is hidden
+  } else if ($("#payment").val() == "credit card" && $("#credit-card").prop("style")[0] == "display") {
+    //Hide all elements
+    $("#payment").nextAll().slideUp();
+    //But the Credit Card
+    $("#payment").next().slideDown();
+  }
+})
+
+// ===== 5th task: Form validation:
+
+//Function to enable submit button
+function enableSubmit() {
+  $("form").children().last().prop("disabled", false);
+  $("form").children().last().removeClass("disabled");
+}
+
+//Function to disable submit button
+function disableSubmit() {
+  $("form").children().last().prop("disabled", true);
+  $("form").children().last().addClass("disabled");
+}
+
+//Function to check if string is empty
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+}
+
+//Function to check if e-mail format is valid
+function emailIsValid(){
+   var regex = new RegExp ("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$");
+   return regex.test($("#mail").val());
+}
+
+//Function to check if role has been selected
+function roleIsSelected() {
+  return $("#title").val() !== null
+}
+
+//Function to check that atleast an activity has been picked
+function activityIsValid(){
+  return ($(".activities input:checked").length > 0)
+}
+
+//Function to check if credit card number is between 13 and 16
+function CCIsValid(){
+  var str = $("#cc-num").val().replace(/-/g, "");
+  console.log(str);
+  return str.length >= 13 && str.length <= 16
+}
+
+//TO MYSELF: MAKE IT SO THAT THESE TWO TAKE ONLY NUMBERS
+//Function to check if ZIP code is valid
+function zipIsValid(){
+  return $("#zip").val().length == 5;
+}
+
+//Function to check if CVV is valid
+function cvvIsValid(){
+  return $("#cvv").val().length == 3;
+}
+
+//After leaving a field
+$("fieldset").change(function(){
+  //If any of this conditions is true
+  if (
+      !isBlank($("#name").val()) &&
+      emailIsValid() &&
+      roleIsSelected() &&
+      activityIsValid()
+    ) {
+  if ($("#title").val() == "other"){
+    if (!isBlank($("#other-title").val())){
+      //If credit card div is displayed
+      if ($("#payment").val() == null || $("#payment").val() == "credit card") {
+          //If the credit card data is correct
+          if (CCIsValid() && zipIsValid() && cvvIsValid()) {
+            //Enable the submit button
+            enableSubmit();
+          //Otherwise disable it
+          } else {
+            disableSubmit();
+          }
+        //Otherwise if users selects other payment methods, enable
+        } else {
+          enableSubmit();
+        }
+      }
+    }
+  }
+});
